e22e535ace1334bdc3d7ecc26aaff30ebf893736
Le Vivilet <levivlet@pm.me>
2024-01-05 22:50:56 +0100

    fix: types for function

--- src/index.d.ts
+++ src/index.d.ts
@@ -3,3 +3,4 @@ export const boolean: (value: any) => void
 export const number: (value: any) => void
 export const object: (value: any) => void
 export const string: (value: any) => void
+export const fn: (value: any) => void
