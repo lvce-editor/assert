16cbf86b7f4341983adc075809238ff4c3015b1f
Le Vivilet <levivlet@pm.me>
2024-01-05 22:48:27 +0100

    feature: add function assertion

--- test/Assert.test.js
+++ test/Assert.test.js
@@ -5,3 +5,15 @@ test('boolean - error', () => {
     new Error(`expected value to be of type boolean`),
   )
 })
+
+test('function', () => {
+  const value = () => {}
+  Assert.fn(value)
+})
+
+test('function - error', () => {
+  const value = 2
+  expect(() => Assert.fn(value)).toThrow(
+    new Error('expected value to be of type function'),
+  )
+})
