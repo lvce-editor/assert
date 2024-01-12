3a37cb06a2f8979fa4b76f0fe70765ddb2ae3080
Le Vivilet <levivlet@pm.me>
2024-01-12 23:11:09 +0100

    refactor: move assertionError class into separate file

--- src/parts/Assert/Assert.js
+++ src/parts/Assert/Assert.js
@@ -1,70 +1,43 @@
-class AssertionError extends Error {
-  constructor(message) {
-    super(message)
-    this.name = 'AssertionError'
-  }
-}
-
-const getType = (value) => {
-  switch (typeof value) {
-    case 'number':
-      return 'number'
-    case 'function':
-      return 'function'
-    case 'string':
-      return 'string'
-    case 'object':
-      if (value === null) {
-        return 'null'
-      }
-      if (Array.isArray(value)) {
-        return 'array'
-      }
-      return 'object'
-    case 'boolean':
-      return 'boolean'
-    default:
-      return 'unknown'
-  }
-}
+import { AssertionError } from '../AssertionError/AssertionError.js'
+import * as GetType from '../GetType/GetType.js'
 
 export const object = (value) => {
-  const type = getType(value)
+  const type = GetType.getType(value)
   if (type !== 'object') {
     throw new AssertionError('expected value to be of type object')
   }
 }
 
 export const number = (value) => {
-  const type = getType(value)
+  const type = GetType.getType(value)
   if (type !== 'number') {
     throw new AssertionError('expected value to be of type number')
   }
 }
 
 export const array = (value) => {
-  const type = getType(value)
+  const type = GetType.getType(value)
   if (type !== 'array') {
     throw new AssertionError('expected value to be of type array')
   }
 }
 
 export const string = (value) => {
-  const type = getType(value)
+  const type = GetType.getType(value)
   if (type !== 'string') {
     throw new AssertionError('expected value to be of type string')
   }
 }
 
 export const boolean = (value) => {
-  const type = getType(value)
+  const type = GetType.getType(value)
   if (type !== 'boolean') {
     throw new AssertionError('expected value to be of type boolean')
   }
 }
 
 export const fn = (value) => {
-  const type = getType(value)
+  const type = GetType.getType(value)
   if (type !== 'function') {
     throw new AssertionError('expected value to be of type function')
   }
